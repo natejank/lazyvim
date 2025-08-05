9ed403fee65cef9633b5ea681e1579284bca21f4
Nathan Jankowski <njj3397@rit.edu>
2025-08-05 14:07:46 -0400

    use nvim-surround instead of mini.surround bc it uses the
    right bindings
     
    also disable flash.nvim because those conflict

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,6 +1,17 @@
 return {
+  -- Disable flash (conflicts with bindings)
+  { "folke/flash.nvim", enabled = false },
   -- Disable mini.surround
   { "echasnovski/mini.surround", enabled = false },
   -- enable nvim-surround because its more similar to vim-surround
-  { "https://github.com/kylechui/nvim-surround" },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  }
 }
